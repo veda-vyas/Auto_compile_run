dbef70181ea31fa8fcd85c9c1359050e85b4417b
niranjan1624 <niranjan.ece1@gmail.com>
2015-09-07 22:04:50 +0530

    Sorted folders according to latest ones and returned all .java files

--- test.py
+++ test.py
@@ -1,15 +1,17 @@
-import os
+import os, time
 import subprocess
+import glob
+import operator
 from subprocess import STDOUT,PIPE
 import re
 
 email_arr = []
 files_arr = []
-file_path = ""
-directory_list = list()
 stdin = ''
 stdout = ''
 stderr = ''
+root_path = "F:\DigitalWallet"
+extension = '.java'
 out = open('output.txt', 'w')
 def execute_java(roots):
     cmd = 'java -cp '+roots+' DigitalWalletHidden'
@@ -27,19 +29,31 @@ def compile_java(roots):
     else:
         out.write("Compilation Errors: \n\n"+stdout+"\n\n")
 ################################################################################
+def NewestDir(directory):
+    os.chdir(directory)
+    dirs = {}
+    for dir in glob.glob('*'):
+        if os.path.isdir(dir):
+            dirs[dir] = os.path.getctime(dir)
+
+    lister = sorted(dirs.iteritems(), key=operator.itemgetter(1))
+    print glob.glob(1)
+    return lister[-1][0]
+
 def return_emails(path):
     for root, dirs, files in os.walk(path, topdown=False):
         if files != []:
             files_arr.append(files)
             email_arr.append((re.search(r'[\w\.-]+@[\w\.-]+', root)).group(0))
-    return email_arr
-print return_emails("F:\DigitalWallet")
+    print email_arr
 
-def return_files(path):
-    for root, dirs, files in os.walk(path, topdown=False):
-        if files != []:
-            files_arr.append(files)
-    return files_arr
-print return_files("F:\DigitalWallet")
+def return_files(email):
+    for root, dirs, files in os.walk(root_path+"\\"+email+"\\"+NewestDir(root_path+"\\"+email), topdown=False): 
+        for name in files:
+            if name.lower().endswith(extension):
+                print root+"\\"+name
+
+return_emails(root_path)
+return_files("ananyaannu22@gmail.com")
 print "Done"
     
